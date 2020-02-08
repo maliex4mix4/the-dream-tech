fb01bdadb36b2e7fda0d49e0a6224ac3ac34dc38
Maliex <malikopeyemi5@gmail.com>
2020-02-08 20:50:44 +0100

    change

--- apiapp/appUsers/views.py
+++ apiapp/appUsers/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-from rest_framework.decorators import app_view, permission_classses
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 
 
-@app_view(['GET'])
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def restricted(request, *args, **kwargs):
 	return Response(data='only for logged in user', status=status.HTTP_200_OK)
