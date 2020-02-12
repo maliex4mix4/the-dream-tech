cad8f766e9b0d384b78b1372420c19a71e3c62a5
Maliex <malikopeyemi5@gmail.com>
2020-02-12 01:37:08 +0100

    rebuilt models

--- apiapp/apiapp/settings.py
+++ apiapp/apiapp/settings.py
@@ -144,4 +144,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-## AUTH_USER_MODEL = 'appUsers.User'
+AUTH_USER_MODEL = 'users.User'
--- apiapp/users/models.py
+++ apiapp/users/models.py
@@ -1,3 +1,21 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+ from .managers import UserManager
+
+ class User(AbstractUser):
+ 	username = None
+ 	phone_no = models.CharField(max_lenght=50, unique=True)
+ 	address = models.CharField(max_lenght=100)
+ 	email = models.EmailField(_('email address'), unique=True)
+
+ 	USERNAME_FIELD = 'email'
+ 	REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'phone_no']
+
+ 	objects = UserManager()
+
+ 	def __str__(self):
+ 		return self.email
