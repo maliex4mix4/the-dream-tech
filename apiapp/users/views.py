e9d212028e3aaf3f0a24fb4e9347a225d3c464c5
Maliex <malikopeyemi5@gmail.com>
2020-02-13 15:17:57 +0100

    changes

--- apiapp/users/views.py
+++ apiapp/users/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
+from django.http import HttpRequest
 
 from .models import User
 
@@ -49,13 +50,10 @@ class LoginView(APIView):
 def req(request):
 		try:
 			data = request.data
+			if len(data) < 1:
+				return Response('Empty', status=status.HTTP_404_NOT_FOUND)
 		except ParseError as error:
 			return Response('Invalid JSON - {0}'.format(error.detail), status=status.HTTP_400_BAD_REQUEST)
-		if "email" not in data or "password" not in data:
-			return Response(
-			'Wrong credentials'+str(request.POST), status=status.HTTP_401_UNAUTHORIZED)
-		user = User.objects.first()
-		if not user:
-			return Response('No default user, please create one', status=status.HTTP_404_NOT_FOUND)
-		token = Token.objects.get_or_create(user=user)
-		return Response({'token': token[0].key})
+		
+		return Response('Wrong credentials '+str(data), status=status.HTTP_401_UNAUTHORIZED)
+		
