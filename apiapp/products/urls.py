f3b06fd18b375e49b6f5e47358b8282a6f5dac93
Maliex <malikopeyemi5@gmail.com>
2020-02-27 13:16:52 +0100

    changes

--- apiapp/products/urls.py
+++ apiapp/products/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import ProductsView
 from .views import ProductsList
+from .views import CategoryView
+from .views import CategoryList
 
 urlpatterns = [
     path('<int:pk>', ProductsList.as_view(), name='add_product'),
-    path('', ProductsView.as_view(), name='add_product'),
+    path('', ProductsView.as_view(), name='view_product'),
+    path('<int:pk>', CategoryList.as_view(), name='add_category'),
+    path('', CategoryView.as_view(), name='view_category'),
 ]
--- apiapp/products/views.py
+++ apiapp/products/views.py
@@ -23,11 +23,11 @@ class ProductsView(APIView):
 
     def post(self, request, *args, **kwargs):
         try:
-			data = request.data
-			if len(data) < 1:
-				data = request.query_params
-		except ParseError as error:
-			return Response({"success": False, "payload": error.detail,}, status=status.HTTP_400_BAD_REQUEST)
+            data = request.data
+            if len(data) < 1:
+            	data = request.query_params
+        except ParseError as error:
+        	return Response({"success": False, "payload": error.detail,}, status=status.HTTP_400_BAD_REQUEST)
 
         product_serializer = ProductsSerializers(data=data)
 
@@ -56,6 +56,55 @@ class ProductsList(APIView):
     def put(self, request, pk):
         obj = self.get_obj(pk)
         serializer_ = ProductsSerializers(obj)
+        if serializer_.is_valid():
+            serializer_.save()
+            return Response({"success": True, "payload": serializer_.data, "info": "Updated successfuly."})
+    def delete(self, request, pk):
+        obj = self.get_obj(pk)
+        obj.delete()
+        return Response({"success": True, "payload": {}, "info": "successful."})
+
+
+class CategoryView(APIView):
+    def get(self, request, format=None):
+        ctgry = Category.objects.all()
+        serializer_ = ProductsSerializers(ctgry, many=True)
+        return Response({"success": True, "payload": serializer_.data,})
+
+    def post(self, request, *args, **kwargs):
+        try:
+        	data = request.data
+        	if len(data) < 1:
+        		data = request.query_params
+        except ParseError as error:
+        	return Response({"success": False, "payload": error.detail,}, status=status.HTTP_400_BAD_REQUEST)
+
+        cat_serializer = CategorySerializers(data=data)
+
+        if cat_serializer.is_valid():
+            cat_serializer.save()
+            return Response({"success": True, "payload": {cat_serializer.initial_data}, "info": "Product added"})
+
+        else:
+            return Response({"success": False, "payload": {}, "info": "Invalid Format."})
+
+
+class CategoryList(APIView):
+    def get_obj(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Exception as e:
+            raise Http404
+
+    def get(self, request, pk):
+        ctgry_ = self.get_obj(pk)
+        serializer_ = CategorySerializers(product_)
+        if serializer_:
+            return Response({"success": True, "payload": serializer_.data, "info": "successful."})
+        return Response({"success": False, "payload": {}, "info": "unknown error."})
+    def put(self, request, pk):
+        obj = self.get_obj(pk)
+        serializer_ = CategorySerializers(obj)
         if serializer_.is_valid():
             serializer_.save()
             return Response({"success": True, "payload": serializer_.data, "info": "Updated successfuly."})
